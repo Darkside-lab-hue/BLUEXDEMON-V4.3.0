9674b1f3c0ab90351304e1dea856a2e5fa5e1560
Darkside-lab-hue <ayodelevctotoria@gmail.com>
2025-01-18 16:56:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2347057344983"
+global.pairingNumber = "2349136930948"
 
 
 
@@ -19,11 +19,11 @@ global.anticall = false
 global.autoReadChat = false
 global.alwaysonline = true
 global.autoswview = false
-global.public = true
+global.public = false
 global.autoreact = false
 global.antidelete = false
-global.autotyping = false
-global.autoBio = true
+global.autotyping = true
+global.autoBio = false
 global.antiSpam = true
 global.sign = "𓃵"
 // ☞ ➻ ➽ ☛ 〆 
